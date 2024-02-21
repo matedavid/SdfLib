d9a50a973c6362b66f0899be707e2763706d0620
matedavid <david.mate.lopez@gmail.com>
2024-02-21 23:12:12 +0100

    format

--- src/tools/SdfGI/main.cpp
+++ src/tools/SdfGI/main.cpp
@@ -23,27 +23,26 @@ public:
     MyScene(std::string modelPath, std::string sdfPath, bool normalizeModel) : mModelPath(modelPath), mSdfPath(sdfPath), mNormalizeModel(normalizeModel) {}
 
     void start() override
-	{
+    {
         Window::getCurrentWindow().setBackgroudColor(glm::vec4(0.9, 0.9, 0.9, 1.0));
-
 
         Mesh mesh(mModelPath);
         mModelBBox = mesh.getBoundingBox();
-        
-        if(mNormalizeModel)
+
+        if (mNormalizeModel)
         {
             // Normalize model units
             const glm::vec3 boxSize = mesh.getBoundingBox().getSize();
             glm::vec3 center = mesh.getBoundingBox().getCenter();
-            mesh.applyTransform(glm::scale(glm::mat4(1.0), glm::vec3(2.0f/glm::max(glm::max(boxSize.x, boxSize.y), boxSize.z))) *
-                                            glm::translate(glm::mat4(1.0), -mesh.getBoundingBox().getCenter()));
+            mesh.applyTransform(glm::scale(glm::mat4(1.0), glm::vec3(2.0f / glm::max(glm::max(boxSize.x, boxSize.y), boxSize.z))) *
+                                glm::translate(glm::mat4(1.0), -mesh.getBoundingBox().getCenter()));
         }
-        
+
         // Load Sdf
         std::unique_ptr<SdfFunction> sdfUnique = SdfFunction::loadFromFile(mSdfPath);
         std::shared_ptr<SdfFunction> sdf = std::move(sdfUnique);
         std::shared_ptr<IOctreeSdf> octreeSdf = std::dynamic_pointer_cast<IOctreeSdf>(sdf);
-        if(octreeSdf->hasSdfOnlyAtSurface())
+        if (octreeSdf->hasSdfOnlyAtSurface())
         {
             std::cerr << "The octrees with the isosurface termination rule are not supported in this application" << std::endl;
             exit(1);
@@ -54,46 +53,46 @@ public:
         // Model Render
         {
             mModelRenderer = std::make_shared<RenderMesh>();
-			mModelRenderer->systemName = "Object Mesh";
-			mModelRenderer->start();
+            mModelRenderer->systemName = "Object Mesh";
+            mModelRenderer->start();
             mesh.computeNormals();
-			mModelRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout> {
-										RenderMesh::VertexParameterLayout(GL_FLOAT, 3)
-								}, mesh.getVertices().data(), mesh.getVertices().size());
+            mModelRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout>{
+                                              RenderMesh::VertexParameterLayout(GL_FLOAT, 3)},
+                                          mesh.getVertices().data(), mesh.getVertices().size());
 
-			mModelRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout> {
-										RenderMesh::VertexParameterLayout(GL_FLOAT, 3)
-								}, mesh.getNormals().data(), mesh.getNormals().size());
+            mModelRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout>{
+                                              RenderMesh::VertexParameterLayout(GL_FLOAT, 3)},
+                                          mesh.getNormals().data(), mesh.getNormals().size());
 
-			mModelRenderer->setIndexData(mesh.getIndices());
-			mModelRenderer->setShader(mOctreeGIShader.get());
+            mModelRenderer->setIndexData(mesh.getIndices());
+            mModelRenderer->setShader(mOctreeGIShader.get());
             mModelRenderer->callDraw = false; // Disable the automatic call because we already call the function
             mModelRenderer->callDrawGui = false;
             mModelRenderer->systemName = "Mesh Model";
-			addSystem(mModelRenderer);
+            addSystem(mModelRenderer);
         }
 
         // Plane Render
         {
             mPlaneRenderer = std::make_shared<RenderMesh>();
-			mPlaneRenderer->start();
+            mPlaneRenderer->start();
 
-			// Plane
-			std::shared_ptr<Mesh> plane = PrimitivesFactory::getPlane();
+            // Plane
+            std::shared_ptr<Mesh> plane = PrimitivesFactory::getPlane();
             plane->computeNormals();
 
-			mPlaneRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout> {
-										RenderMesh::VertexParameterLayout(GL_FLOAT, 3)
-								}, plane->getVertices().data(), plane->getVertices().size());
+            mPlaneRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout>{
+                                              RenderMesh::VertexParameterLayout(GL_FLOAT, 3)},
+                                          plane->getVertices().data(), plane->getVertices().size());
 
-            mPlaneRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout> {
-										RenderMesh::VertexParameterLayout(GL_FLOAT, 3)
-								}, plane->getNormals().data(), plane->getNormals().size());
+            mPlaneRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout>{
+                                              RenderMesh::VertexParameterLayout(GL_FLOAT, 3)},
+                                          plane->getNormals().data(), plane->getNormals().size());
 
-			mPlaneRenderer->setIndexData(plane->getIndices());
-			mPlaneRenderer->setTransform(glm::translate(glm::mat4(1.0f), glm::vec3(mesh.getBoundingBox().getCenter().x, mesh.getBoundingBox().min.y, mesh.getBoundingBox().getCenter().z)) * 
-                                         glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)) * 
-                                        glm::scale(glm::mat4(1.0f), glm::vec3(64.0f)));
+            mPlaneRenderer->setIndexData(plane->getIndices());
+            mPlaneRenderer->setTransform(glm::translate(glm::mat4(1.0f), glm::vec3(mesh.getBoundingBox().getCenter().x, mesh.getBoundingBox().min.y, mesh.getBoundingBox().getCenter().z)) *
+                                         glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)) *
+                                         glm::scale(glm::mat4(1.0f), glm::vec3(64.0f)));
             mPlaneRenderer->setShader(mOctreeGIShader.get());
             mPlaneRenderer->callDraw = false; // Disable the automatic call because we already call the function
             mPlaneRenderer->callDrawGui = false;
@@ -110,15 +109,15 @@ public:
             std::shared_ptr<Mesh> cube = PrimitivesFactory::getCube();
             cube->computeNormals();
 
-			mLightRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout> {
-										RenderMesh::VertexParameterLayout(GL_FLOAT, 3)
-								}, cube->getVertices().data(), cube->getVertices().size());
+            mLightRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout>{
+                                              RenderMesh::VertexParameterLayout(GL_FLOAT, 3)},
+                                          cube->getVertices().data(), cube->getVertices().size());
 
-            mLightRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout> {
-										RenderMesh::VertexParameterLayout(GL_FLOAT, 3)
-								}, cube->getNormals().data(), cube->getNormals().size());
+            mLightRenderer->setVertexData(std::vector<RenderMesh::VertexParameterLayout>{
+                                              RenderMesh::VertexParameterLayout(GL_FLOAT, 3)},
+                                          cube->getNormals().data(), cube->getNormals().size());
 
-			mLightRenderer->setIndexData(cube->getIndices());
+            mLightRenderer->setIndexData(cube->getIndices());
 
             mLightRenderer->setShader(mOctreeGIShader.get());
             mLightRenderer->callDraw = false; // Disable the automatic call because we already call the function
@@ -139,12 +138,11 @@ public:
         setMainCamera(camera);
         addSystem(camera);
         mCamera = camera;
-
     }
 
     void update(float deltaTime) override
-	{
-        auto& wnd = Window::getCurrentWindow();
+    {
+        auto &wnd = Window::getCurrentWindow();
         const bool shouldStopIndirect = wnd.isMouseButtonPressed(GLFW_MOUSE_BUTTON_RIGHT);
 
         if (shouldStopIndirect)
@@ -156,7 +154,7 @@ public:
     bool mDrawGui = true;
     virtual void draw() override
     {
-        //Lights
+        // Lights
         for (int i = 0; i < mLightNumber; i++)
         {
             mOctreeGIShader->setLightInfo(i, mLightPosition[i], mLightColor[i], mLightIntensity[i], mLightRadius[i]);
@@ -171,7 +169,7 @@ public:
             mLightRenderer->draw(getMainCamera());
         }
 
-        //Model
+        // Model
         mOctreeGIShader->setMaterial(mAlbedo, mRoughness, mMetallic, mF0);
         mOctreeGIShader->setLightNumber(mLightNumber);
 
@@ -185,7 +183,7 @@ public:
 
         mModelRenderer->draw(getMainCamera());
 
-        //Plane
+        // Plane
         mOctreeGIShader->setMaterial(glm::vec3(0.7), 0.1, 0.1, glm::vec3(0.07));
         mPlaneRenderer->draw(getMainCamera());
 
@@ -194,13 +192,13 @@ public:
         Scene::draw();
     }
 
-    void drawGui() 
+    void drawGui()
     {
-        if (ImGui::BeginMainMenuBar()) 
+        if (ImGui::BeginMainMenuBar())
         {
-            if (ImGui::BeginMenu("Scene")) 
+            if (ImGui::BeginMenu("Scene"))
             {
-                ImGui::MenuItem("Show scene settings", NULL, &mShowSceneGUI);                
+                ImGui::MenuItem("Show scene settings", NULL, &mShowSceneGUI);
                 ImGui::EndMenu();
             }
 
@@ -209,12 +207,12 @@ public:
 
         mCamera->drawGuiWindow();
 
-        if (mShowSceneGUI) 
+        if (mShowSceneGUI)
         {
             ImGui::Begin("Scene");
             // Print light settings
             ImGui::Spacing();
-		    ImGui::Separator();
+            ImGui::Separator();
 
             ImGui::Text("Global Illumination Settings");
             ImGui::Checkbox("Use Indirect", &mUseIndirect);
@@ -231,7 +229,7 @@ public:
                 const auto modelPath = std::filesystem::current_path() / mModelPath;
                 const auto bboxSize = mModelBBox.getSize();
 
-                const auto scale = 2.0f/glm::max(glm::max(bboxSize.x, bboxSize.y), bboxSize.z);
+                const auto scale = 2.0f / glm::max(glm::max(bboxSize.x, bboxSize.y), bboxSize.z);
                 const auto translate = -mModelBBox.getCenter();
 
                 exporter.addModel({
@@ -257,31 +255,32 @@ public:
             ImGui::Text("Lighting settings");
             ImGui::SliderInt("Lights", &mLightNumber, 1, 4);
 
-            for (int i = 0; i < mLightNumber; ++i) { //DOES NOT WORK, PROBLEM WITH REFERENCES
+            for (int i = 0; i < mLightNumber; ++i)
+            { // DOES NOT WORK, PROBLEM WITH REFERENCES
                 ImGui::Text("Light %d", i);
-                std::string pos = "Position##"+std::to_string(i+48);
-                std::string col = "Color##"+std::to_string(i+48);
-                std::string intens = "Intensity##"+std::to_string(i+48);
-                std::string radius = "Radius##"+std::to_string(i+48);
-                ImGui::InputFloat3(pos.c_str(), reinterpret_cast<float*>(&mLightPosition[i]));
-                ImGui::ColorEdit3(col.c_str(), reinterpret_cast<float*>(&mLightColor[i]));
+                std::string pos = "Position##" + std::to_string(i + 48);
+                std::string col = "Color##" + std::to_string(i + 48);
+                std::string intens = "Intensity##" + std::to_string(i + 48);
+                std::string radius = "Radius##" + std::to_string(i + 48);
+                ImGui::InputFloat3(pos.c_str(), reinterpret_cast<float *>(&mLightPosition[i]));
+                ImGui::ColorEdit3(col.c_str(), reinterpret_cast<float *>(&mLightColor[i]));
                 ImGui::SliderFloat(intens.c_str(), &mLightIntensity[i], 0.0f, 20.0f);
                 ImGui::SliderFloat(radius.c_str(), &mLightRadius[i], 0.01f, 1.0f);
             }
 
             // Print meterial settings
             ImGui::Spacing();
-		    ImGui::Separator();
+            ImGui::Separator();
 
             ImGui::Text("Material settings");
             ImGui::SliderFloat("Metallic", &mMetallic, 0.0f, 1.0f);
             ImGui::SliderFloat("Roughness", &mRoughness, 0.0f, 1.0f);
-            ImGui::ColorEdit3("Albedo", reinterpret_cast<float*>(&mAlbedo));
-            ImGui::ColorEdit3("F0", reinterpret_cast<float*>(&mF0));
+            ImGui::ColorEdit3("Albedo", reinterpret_cast<float *>(&mAlbedo));
+            ImGui::ColorEdit3("F0", reinterpret_cast<float *>(&mF0));
 
             // Print algorithm settings
             ImGui::Spacing();
-		    ImGui::Separator();
+            ImGui::Separator();
 
             ImGui::Text("Algorithm Settings");
             ImGui::InputInt("Max Shadow Iterations", &mMaxShadowIterations);
@@ -301,15 +300,16 @@ public:
 
     void takeScreenshot()
     {
-        auto& window = Window::getCurrentWindow();
+        auto &window = Window::getCurrentWindow();
 
-        const auto& [width, height] = [&]() {
+        const auto &[width, height] = [&]()
+        {
             const auto size = window.getWindowSize();
             return std::make_pair(size.x, size.y);
         }();
 
-        const int screenshotWidth = 1920;
-        const int screenshotHeight = 1080;
+        constexpr int screenshotWidth = 1920;
+        constexpr int screenshotHeight = 1080;
         window.setWindowSize(glm::ivec2(screenshotWidth, screenshotHeight));
 
         resize(glm::ivec2(screenshotWidth, screenshotHeight));
@@ -336,16 +336,17 @@ public:
         mMaxDepth = prevMaxDepth;
 
         {
-            unsigned char* pixels = new unsigned char[screenshotWidth * screenshotHeight * 3];
+            unsigned char *pixels = new unsigned char[screenshotWidth * screenshotHeight * 3];
 
             glReadBuffer(GL_COLOR_ATTACHMENT0);
             glReadPixels(0, 0, screenshotWidth, screenshotHeight, GL_RGB, GL_UNSIGNED_BYTE, pixels);
 
             std::ofstream file("screenshot.ppm");
 
-            file << "P3\n" << screenshotWidth << " " << screenshotHeight << "\n255\n";
+            file << "P3\n"
+                 << screenshotWidth << " " << screenshotHeight << "\n255\n";
 
-            for (int row = screenshotHeight-1; row >= 0; --row)
+            for (int row = screenshotHeight - 1; row >= 0; --row)
             {
                 for (int col = 0; col < screenshotWidth; ++col)
                 {
@@ -378,80 +379,76 @@ private:
 
     std::unique_ptr<SdfOctreeGIShader> mOctreeGIShader;
 
-    //Options
+    // Options
     int mMaxShadowIterations = 512;
     bool mUseSoftShadows = true;
 
-    //Global Illumination Settings
+    // Global Illumination Settings
     bool mUseIndirect = false;
     int mNumSamples = 2;
     int mMaxDepth = 1;
     int mMaxRaycastIterations = 50;
 
-    //Lighting
+    // Lighting
     int mLightNumber = 1;
     glm::vec3 mLightPosition[4] =
-    {
-        glm::vec3 (1.0f, 2.0f, 1.0f),
-        // glm::vec3 (0.0f, 0.2f, -1.5f),
-        glm::vec3 (-1.0f, 2.0f, 1.0f),
-        glm::vec3 (1.0f, 2.0f, -1.0f),
-        glm::vec3 (-1.0f, 2.0f, -1.0f)
-    };
+        {
+            glm::vec3(1.0f, 2.0f, 1.0f),
+            // glm::vec3 (0.0f, 0.2f, -1.5f),
+            glm::vec3(-1.0f, 2.0f, 1.0f),
+            glm::vec3(1.0f, 2.0f, -1.0f),
+            glm::vec3(-1.0f, 2.0f, -1.0f)};
 
     glm::vec3 mLightColor[4] =
-    {
-        glm::vec3(1.0f, 1.0f, 1.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f),
-        glm::vec3(0.0f, 0.0f, 1.0f),
-        glm::vec3(1.0f, 0.0f, 1.0f)
-    };
+        {
+            glm::vec3(1.0f, 1.0f, 1.0f),
+            glm::vec3(0.0f, 1.0f, 0.0f),
+            glm::vec3(0.0f, 0.0f, 1.0f),
+            glm::vec3(1.0f, 0.0f, 1.0f)};
 
-    float mLightIntensity[4] = 
-    {
-        10.0f,
-        10.0f,
-        10.0f,
-        10.0f
-    };
+    float mLightIntensity[4] =
+        {
+            10.0f,
+            10.0f,
+            10.0f,
+            10.0f};
 
     float mLightRadius[4] =
-    {
-        0.1f,
-        0.1f,
-        0.1f,
-        0.1f
-    };
+        {
+            0.1f,
+            0.1f,
+            0.1f,
+            0.1f};
 
-    //Material
+    // Material
     float mMetallic = 0.0f;
     float mRoughness = 0.5f;
     glm::vec3 mAlbedo = glm::vec3(0.3f, 0.3f, 0.3f);
     glm::vec3 mF0 = glm::vec3(0.07f, 0.07f, 0.07f);
 
-    //GUI
+    // GUI
     bool mShowSceneGUI = false;
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    #ifdef SDFLIB_PRINT_STATISTICS
-        spdlog::set_pattern("[%^%l%$] [%s:%#] %v");
-    #else
-        spdlog::set_pattern("[%^%l%$] %v");
-    #endif
+#ifdef SDFLIB_PRINT_STATISTICS
+    spdlog::set_pattern("[%^%l%$] [%s:%#] %v");
+#else
+    spdlog::set_pattern("[%^%l%$] %v");
+#endif
 
     args::ArgumentParser parser("SdfLight app for rendering a model using its sdf");
     args::HelpFlag help(parser, "help", "Display help menu", {'h', "help"});
     args::Positional<std::string> modelPathArg(parser, "model_path", "The model path");
     args::Positional<std::string> sdfPathArg(parser, "sdf_path", "The sdf model path");
     args::Flag normalizeBBArg(parser, "sdf_is_normalized", "Indicates that the sdf model is normalized", {'n', "normalize"});
-    
+
     try
     {
         parser.ParseCLI(argc, argv);
     }
-    catch(args::Help)
+    catch (args::Help)
     {
         std::cerr << parser;
         return 0;
